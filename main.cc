5d69574b305f0c3893d9e3ed287102877ecf9eea
Patrick Flynn <patrick.flynn.ctr@nrlmry.navy.mil>
2021-11-13 06:56:47 -0800

    first github action attempt

--- main.cc
+++ main.cc
@@ -1,8 +1,28 @@
 #include<iostream>
+#include<string>
+
 using namespace std;
-int main()
+
+int main(int argc, char *argv[])
 {
-	cout << "Hello there!\n";
-    return 0;
+   const string succeed = "succeed";
+   const string fail    = "fail";
+   const int SUCCEED    = 0;
+   const int FAIL       = 1;
+
+   if (argc != 2) {
+     cout << "Need one argument: main succeed   or   main fail\n";
+     return FAIL;
+   }
+
+   string argument(argv[1]);
+
+   if (argument == succeed) {
+      cout << "succeed\n";
+      return SUCCEED;
+   } else {
+      cout << "failed\n";
+      return FAIL;
+   }
 }
 
